4863da28627da72162f15b5d618370a4d2f4c0e6
MT2321 <42226186+MT2321@users.noreply.github.com>
2018-08-20 14:42:48 -0300

    Add files via upload
    Ejercicio 1 del TP N°1 de Electronica 3. Se implemento un programa que calcule Rango y Resolución de un número de punto de fijo

---
+++ ej1/main.py
@@ -0,0 +1,59 @@
+import argparse
+
+def parseArgs():
+    parser = argparse.ArgumentParser(description='Compute Range and Resolution of Fixed Point Number.', add_help = True) 
+    parser.add_argument('parameters', type = checkInput ,nargs='+',help = "SIGNO  INTBITS  FRACBITS") #checkInput is a custome check function. See below 
+    args = parser.parse_args()
+    validInput = checkArgs(args)  
+    
+    if validInput:
+        return args
+    else:
+        return False
+# Validation of raw input data (param is received as a string). Must be non-negative integer.
+def checkInput(param):
+    try:
+        return int(param) #Return converted parameter
+    except:
+        return -1    #Return -1 if dataType doesn't match
+
+#If datatypes are correct procced with arguments validation.
+def checkArgs(args):
+    argsOk = True
+    #Check quantity of parameters 
+    if(len(args.parameters) > 3):
+        print("WARNING ONLY THE FIRST 3 TERMS WILL BE CONSIDERED")  
+    if(len(args.parameters) < 3):
+        argsOk = False
+
+    for a in args.parameters:
+        if (a < 0):
+            argsOk = False
+            
+    if(args.parameters[0] >= 2):
+        argsOk = False
+
+    return argsOk
+
+
+
+
+#Computes Range and Resolution. 
+def process(args):
+    intBits = args.parameters[1]
+    fracBits = args.parameters[2]
+    rango = 2**intBits  - 0.5**fracBits
+    resolucion = 0.5**fracBits
+    print("\nRes:\t"+ str(resolucion) + " | Ran:\t"+str(rango)+"\n")
+
+def main():
+    args = parseArgs()
+    if args != False: #If everything is valid then compute
+        process(args)
+    else:
+        print("ERROR")
+        return
+
+if __name__ == "__main__":
+    main()
+    
---
+++ ej1/testBench.sh
@@ -0,0 +1,25 @@
+python mainv2.py 0 1 1 
+python mainv2.py 1 1 1 
+python mainv2.py 1 2 3 
+python mainv2.py 0 10 20 
+
+python mainv2.py -1 1 1 
+python mainv2.py -1 -1 1 
+python mainv2.py 1 1 -1
+python mainv2.py a b c
+python mainv2.py 1 a b
+python mainv2.py a 1 c
+python mainv2.py a b 1
+
+python mainv2.py -1 a b
+python mainv2.py a -1 c
+python mainv2.py a b -1
+
+python mainv2.py 1 2 3 4 5 6 #Only the first 3 will be taken into account
+python mainv2.py 1 1 	 
+
+
+python mainv2.py 1 -1 3 4 5 6 #Only the first 3 will be taken into account
+python mainv2.py 1 -1 	
+ 
+read
