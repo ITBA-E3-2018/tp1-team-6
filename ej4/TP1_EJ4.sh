7c2f603300562e01acd82ace6cddd11cdc8517ba
JuanMartinLaguinge <42174219+JuanMartinLaguinge@users.noreply.github.com>
2018-08-25 15:01:07 -0300

    Add files via upload

---
+++ ej4/TP1_EJ4.sh
@@ -0,0 +1,18 @@
+iverilog TP1_EJ4.v
+echo Tabla de Verdad
+vvp a.out +x=0000 rm a.out
+vvp a.out +x=0001 rm a.out
+vvp a.out +x=0010 rm a.out
+vvp a.out +x=0011 rm a.out
+vvp a.out +x=0100 rm a.out
+vvp a.out +x=0101 rm a.out
+vvp a.out +x=0110 rm a.out
+vvp a.out +x=0111 rm a.out
+vvp a.out +x=1000 rm a.out
+vvp a.out +x=1001 rm a.out
+vvp a.out +x=1010 rm a.out
+vvp a.out +x=1011 rm a.out
+vvp a.out +x=1100 rm a.out
+vvp a.out +x=1101 rm a.out
+vvp a.out +x=1110 rm a.out
+vvp a.out +x=1111 rm a.out
