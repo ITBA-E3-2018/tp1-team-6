98f1724f9e16a4027f839c2d54333e49db70c258
JuanMartinLaguinge <42174219+JuanMartinLaguinge@users.noreply.github.com>
2018-09-01 20:10:13 -0300

    Banco de pruebas puro, le quite lo de compilacion

--- ej4/TP1_EJ4.sh
+++ ej4/TP1_EJ4.sh
@@ -1,19 +1,17 @@
-iverilog TP1_EJ4.v
-#Realizamos un testeo de la tabla completa
 echo Tabla de Verdad
-vvp a.out +x=0000 rm a.out
-vvp a.out +x=0001 rm a.out
-vvp a.out +x=0010 rm a.out
-vvp a.out +x=0011 rm a.out
-vvp a.out +x=0100 rm a.out
-vvp a.out +x=0101 rm a.out
-vvp a.out +x=0110 rm a.out
-vvp a.out +x=0111 rm a.out
-vvp a.out +x=1000 rm a.out
-vvp a.out +x=1001 rm a.out
-vvp a.out +x=1010 rm a.out
-vvp a.out +x=1011 rm a.out
-vvp a.out +x=1100 rm a.out
-vvp a.out +x=1101 rm a.out
-vvp a.out +x=1110 rm a.out
-vvp a.out +x=1111 rm a.out
+vvp ./execute/TP1_EJ4 +x=0000 rm ./execute/TP1_EJ4
+vvp ./execute/TP1_EJ4 +x=0001 rm ./execute/TP1_EJ4
+vvp ./execute/TP1_EJ4 +x=0010 rm ./execute/TP1_EJ4
+vvp ./execute/TP1_EJ4 +x=0011 rm ./execute/TP1_EJ4
+vvp ./execute/TP1_EJ4 +x=0100 rm ./execute/TP1_EJ4
+vvp ./execute/TP1_EJ4 +x=0101 rm ./execute/TP1_EJ4
+vvp ./execute/TP1_EJ4 +x=0110 rm ./execute/TP1_EJ4
+vvp ./execute/TP1_EJ4 +x=0111 rm ./execute/TP1_EJ4
+vvp ./execute/TP1_EJ4 +x=1000 rm ./execute/TP1_EJ4
+vvp ./execute/TP1_EJ4 +x=1001 rm ./execute/TP1_EJ4
+vvp ./execute/TP1_EJ4 +x=1010 rm ./execute/TP1_EJ4
+vvp ./execute/TP1_EJ4 +x=1011 rm ./execute/TP1_EJ4
+vvp ./execute/TP1_EJ4 +x=1100 rm ./execute/TP1_EJ4
+vvp ./execute/TP1_EJ4 +x=1101 rm ./execute/TP1_EJ4
+vvp ./execute/TP1_EJ4 +x=1110 rm ./execute/TP1_EJ4
+vvp ./execute/TP1_EJ4 +x=1111 rm ./execute/TP1_EJ4
